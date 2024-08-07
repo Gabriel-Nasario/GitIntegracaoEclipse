02c9f06987c153fc66f6422b052ee4281caa7b6c
Gabriel-Nasario <gabrielnabnu@gmail.com>
2024-08-07 16:24:36 -0300

    Update TesteGitEclipse.java

--- IntegracaoGirEclipse/src/Git_teste/TesteGitEclipse.java
+++ IntegracaoGirEclipse/src/Git_teste/TesteGitEclipse.java
@@ -7,6 +7,7 @@ public class TesteGitEclipse {
 		System.out.println("Integração Git Eclipse Primeiro Teste.");
 		System.out.println("Alterando do Git para o Eclipse.");
 		System.out.println("Segundo commit.");
+		System.out.println("Amanha");
 		
 	}
 }
